3ea59bbebe28ecda9ce623814a529b41c4450118
Tiago Nobrega <tigarmo@gmail.com>
2020-11-06 19:18:46 -0300

    Some more vec ops, some more tests

--- src/geo/vec3.rs
+++ src/geo/vec3.rs
@@ -13,6 +13,13 @@ impl Vec3 {
     }
 }
 
+// This one is just to simplify tests
+impl PartialEq<(f64, f64, f64)> for Vec3 {
+    fn eq(&self, other: &(f64, f64, f64)) -> bool {
+        self.x == other.0 && self.y == other.1 && self.z == other.2
+    }
+}
+
 impl ops::Add for Vec3 {
     type Output = Self;
 
@@ -49,23 +56,56 @@ impl ops::SubAssign for Vec3 {
     }
 }
 
+impl ops::Mul<f64> for Vec3 {
+    type Output = Self;
+    fn mul(self, rhs: f64) -> Vec3 {
+        let mut p = self;
+        p *= rhs;
+        p
+    }
+}
+
+impl ops::MulAssign<f64> for Vec3 {
+    fn mul_assign(&mut self, rhs: f64) {
+        self.x *= rhs;
+        self.y *= rhs;
+        self.z *= rhs;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Vec3;
-    #[test]
-    fn test_ops() {
-        let p1 = Vec3::new(1.0, 2.0, 3.0);
-        let p2 = Vec3::new(-1.0, -2.0, -3.0);
 
-        assert_eq!(p1 + p2, Vec3::new(0.0, 0.0, 0.0));
+    fn setup() -> (Vec3, Vec3) {
+        (Vec3::new(1.0, 2.0, 3.0), Vec3::new(-1.0, -2.0, -3.0))
+    }
+    #[test]
+    fn test_add() {
+        let (p1, p2) = setup();
+        assert_eq!(p1 + p2, (0.0, 0.0, 0.0));
+        assert_eq!(p2 + p1, (0.0, 0.0, 0.0));
         let mut p3 = p1;
         p3 += p2;
-        assert_eq!(p3, Vec3::new(0.0, 0.0, 0.0));
+        assert_eq!(p3, (0.0, 0.0, 0.0));
+    }
 
-        assert_eq!(p1 - p2, Vec3::new(2.0, 4.0, 6.0));
-        assert_eq!(p2 - p1, Vec3::new(-2.0, -4.0, -6.0));
+    #[test]
+    fn test_sub() {
+        let (p1, p2) = setup();
+        assert_eq!(p1 - p2, (2.0, 4.0, 6.0));
+        assert_eq!(p2 - p1, (-2.0, -4.0, -6.0));
         let mut p4 = p1;
         p4 -= p2;
-        assert_eq!(p4, Vec3::new(2.0, 4.0, 6.0));
+        assert_eq!(p4, (2.0, 4.0, 6.0));
+    }
+
+    #[test]
+    fn test_mul() {
+        let (p1, p2) = setup();
+        assert_eq!(p1 * 2.0, (2.0, 4.0, 6.0));
+        let mut p3 = p2;
+        p3 *= 3.0;
+        assert_eq!(p3, (-3.0, -6.0, -9.0));
     }
 }
